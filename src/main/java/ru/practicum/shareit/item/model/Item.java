a81567d4eff8088029531a0fc0e44be20e45dbb8
Egor Malyshev <ya.krumm@ya.ru>
2025-07-25 12:04:13 +0300

    #1 созданы модели объектов user и item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,29 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.booking.model.BookingStatus;
+import ru.practicum.shareit.request.ItemRequest;
+import ru.practicum.shareit.user.model.ext.Owner;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class Item {
+	
+	private Long id;
+	private String name;
+	private String description;
+	private ItemStatus itemStatus;
+	private Owner owner;
+	private ItemRequest request;
+	
+	
+	
+	
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.request;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import ru.practicum.shareit.user.model.ext.Booker;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class ItemRequest {
+	private Long id;
+	private String description;
+	private Booker requestor;
+	private LocalDateTime created;
 }
