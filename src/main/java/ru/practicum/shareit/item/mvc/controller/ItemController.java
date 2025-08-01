d066873fbb2cb818d380efea441c5ce6281d5c7a
Egor Malyshev <ya.krumm@ya.ru>
2025-08-01 22:53:15 +0300

    #14 добавлен метод обновления предмета

--- src/main/java/ru/practicum/shareit/item/mvc/controller/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/mvc/controller/ItemController.java
@@ -2,19 +2,25 @@ package ru.practicum.shareit.item.mvc.controller;
 
 import java.util.List;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.practicum.shareit.item.dto.CreateItemDto;
 import ru.practicum.shareit.item.dto.ResponseItemDto;
+import ru.practicum.shareit.item.dto.UpdateItemDto;
 
 @Slf4j
-@RestController("/items")
+@RestController
+@RequestMapping("/items")
 public class ItemController {
 
 	private final ItemService itemService;
@@ -24,31 +30,33 @@ public class ItemController {
 	}
 
 	@PostMapping
-	public ResponseItemDto createItem(@RequestHeader("X-Sharer-User-Id") Long ownerId, CreateItemDto itemDto) {
+	public ResponseItemDto createItem(
+			@RequestHeader("X-Sharer-User-Id") Long ownerId, 
+			@Validated @RequestBody CreateItemDto itemDto) {
 		return itemService.createItem(itemDto, ownerId);
 	}
 
+	@PatchMapping("/{id}")
+	public ResponseItemDto updateItem(
+			@RequestHeader("X-Sharer-User-Id") Long ownerId,
+			@RequestBody UpdateItemDto itemDto,
+			@PathVariable("id") Long itemId) {
+	    log.warn("HERE UPDATE");
+	    return itemService.updateItem(itemDto, ownerId, itemId);
+	}
 	@GetMapping("/{id}")
-	public ResponseItemDto getItem(@RequestParam("id") Long itemId) {
+	public ResponseItemDto getItem(@PathVariable("id") Long itemId) {
 		return itemService.getItem(itemId);
 	}
-
 
 	@DeleteMapping
 	public List<ResponseItemDto> deleteAllItems() {
 		return itemService.deleteAllItems();
 	}
 
-//	@PatchMapping("/{itemId}")
-//	public ResponseItemDto updateItem(@RequestHeader("X-Sharer-User-Id") Long userId, CreateItemDto itemDto) {
-//		log.info("Начато обновление предмета. Получен объект:" + itemDto + " и id-пользователя=" + userId);
-//		return itemService.updateItem(itemDto, userId);
-//	}
-
 //	@GetMapping("/search?text={text}")
 //	public List<ResponseItemDto> searchItemByText(@PathVariable("text") String text) {
 //		log.info("Начат поиск предмета. Получена строка:" + text);
 //		return itemService.searchItemByText(text);
 //	}
-
 }
