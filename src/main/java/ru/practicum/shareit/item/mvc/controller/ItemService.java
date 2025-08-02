84b3ebf8173e3e30683e19d167629e7c1290707e
Egor Malyshev <ya.krumm@ya.ru>
2025-08-02 18:24:14 +0300

    #18 обновлена модель передачи данных для предметов посредством dto

--- src/main/java/ru/practicum/shareit/item/mvc/controller/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/mvc/controller/ItemService.java
@@ -8,37 +8,35 @@ import org.springframework.stereotype.Service;
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
 import ru.practicum.shareit.item.dto.CreateItemDto;
+import ru.practicum.shareit.item.dto.FindItemDto;
 import ru.practicum.shareit.item.dto.ResponseItemDto;
 import ru.practicum.shareit.item.dto.UpdateItemDto;
 import ru.practicum.shareit.item.exception.ItemException;
 import ru.practicum.shareit.user.exception.UserException;
-import ru.practicum.shareit.user.mvc.controller.UserRepository;
 
 @Slf4j
 @Service
 public class ItemService {
 
-	private final UserRepository userRepository;
-
 	private final ItemRepository itemRepository;
 	private final UserException userException;
 	private final ItemException itemException;
 
-	public ItemService(ItemRepository itemRepositry, UserException userException, ItemException itemException,
-			UserRepository userRepository) {
+	public ItemService(ItemRepository itemRepositry, UserException userException, ItemException itemException) {
 		this.itemException = itemException;
 		this.userException = userException;
 		this.itemRepository = itemRepositry;
-		this.userRepository = userRepository;
 	}
 
-	public ResponseItemDto createItem(@NotNull CreateItemDto itemDto, Long ownerId) {
+	public ResponseItemDto createItem(@NotNull CreateItemDto createItemDto) {
+		Long ownerId = createItemDto.getOwnerId();
+
 		String errorMessage = "Невозможно создать предмет.";
 		userException.checkUserNotFoundException(ownerId, errorMessage);
 
-		log.info("Начато создание предмета. Получен объект:" + itemDto + " и id-владельца=" + ownerId);
-		ResponseItemDto createdItem = itemRepository.createItem(itemDto, ownerId);
-		log.info("Создан предмет " + createdItem);
+		log.info("Начато создание предмета. Получен объект:" + createItemDto);
+		ResponseItemDto createdItem = itemRepository.createItem(createItemDto);
+		log.info("Создан предмет: " + createdItem);
 
 		Long itemId = createdItem.getId();
 		setOwnerToItem(itemId, ownerId);
@@ -46,13 +44,16 @@ public class ItemService {
 		return createdItem;
 	}
 	
-	public ResponseItemDto updateItem(UpdateItemDto itemDto, Long ownerId, Long itemId) {
+	public ResponseItemDto updateItem(UpdateItemDto updateItemDto) {
+		Long ownerId = updateItemDto.getOwnerId();
+		Long itemId = updateItemDto.getItemId();
+
 		String errorMessage = "Невозможно обновить предмет";
 		itemException.checkItemNotFoundException(itemId, errorMessage);
 		userException.checkUserNotFoundException(ownerId, errorMessage);
 
-		log.info("Начато обновление предмета. Получен объект:" + itemDto + ", ownerId:" + ownerId +", itemId:" + itemId);
-		ResponseItemDto updatedItem = itemRepository.updateItem(itemDto, itemId);
+		log.info("Начато обновление предмета. Получен объект:" + updateItemDto);
+		ResponseItemDto updatedItem = itemRepository.updateItem(updateItemDto);
 		log.info("Обновлен предмет " + updatedItem);
 		return updatedItem;
 	}
@@ -104,15 +105,18 @@ public class ItemService {
 		return itemsOfUserList;
 	}
 
-	public List<ResponseItemDto> searchItemByText(String text, Long ownerId) {
+	public List<ResponseItemDto> searchItemByText(FindItemDto findItemDto) {
+		Long ownerId = findItemDto.getOwnerId();
+		String text = findItemDto.getText();
+
 		String errorMessage = "Невозможно начать поиск предмета владельца по строке поиска";
 		userException.checkUserNotFoundException(ownerId, errorMessage);
-		if (text.isBlank()) {
+
+		if (findItemDto.getText().isBlank()) {
 			return new ArrayList<ResponseItemDto>();
 		}
 
-		log.info("Начат поиск предмета. Получен id-владельца и строка поиска:" + text);
-		return itemRepository.searchItemByText(text, ownerId);
+		log.info("Начат поиск предмета. Получен id-владельца: " + ownerId + " и строка поиска:" + text);
+		return itemRepository.searchItemByText(findItemDto);
 	}
-
 }
