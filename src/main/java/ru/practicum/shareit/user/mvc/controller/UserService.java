6089d9f2affa6ec3d5d7d07be37f71ea43a6a290
Egor Malyshev <ya.krumm@ya.ru>
2025-08-04 20:08:37 +0300

    #21 удалены классы UserRepository и ItemRepository. Созданы их интерфейсы

--- src/main/java/ru/practicum/shareit/user/mvc/controller/UserService.java
+++ src/main/java/ru/practicum/shareit/user/mvc/controller/UserService.java
@@ -6,21 +6,24 @@ import org.springframework.stereotype.Service;
 
 import jakarta.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
-import ru.practicum.shareit.item.mvc.controller.ItemRepository;
+import ru.practicum.shareit.item.mvc.controller.ItemRepositoryApp;
 import ru.practicum.shareit.user.dto.CreateUserDto;
 import ru.practicum.shareit.user.dto.ResponseUserDto;
 import ru.practicum.shareit.user.dto.UpdateUserDto;
 import ru.practicum.shareit.user.exception.UserException;
+import ru.practicum.shareit.user.mvc.model.User;
+import ru.practicum.shareit.user.utils.ResponseUserDtoMapper;
+import ru.practicum.shareit.user.utils.UserMapper;
 
 @Slf4j
 @Service
 public class UserService {
 
 	private final UserException userException;
-	private final UserRepository userRepository;
-	private final ItemRepository itemRepository;
+	private final UserRepositoryApp userRepository;
+	private final ItemRepositoryApp itemRepository;
 
-	public UserService(UserException userValidator, UserRepository userRepository, ItemRepository itemRepository) {
+	public UserService(UserException userValidator, UserRepositoryApp userRepository, ItemRepositoryApp itemRepository) {
 		this.userException = userValidator;
 		this.userRepository = userRepository;
 		this.itemRepository = itemRepository;
@@ -30,25 +33,43 @@ public class UserService {
 		String errorMessage = "Невозможно создать пользователя.";
 		userException.checkEmailAlreadyExistsException(createUserDto.getEmail(), errorMessage);
 
-		log.info("Начато создание пользователя. Получен объект: " + createUserDto);
-		ResponseUserDto createdUser = userRepository.createUser(createUserDto);
-		log.info("Создан пользователь: " + createdUser);
-		return createdUser;
+		log.info("Начато преобразование createUserDto в объект класса User. Получен объект: " + createUserDto);
+		User createuser = UserMapper.createUserDtoToUser(createUserDto);
+		log.info("createUserDto преобразован в объект класса User: " + createuser);
+		
+		log.info("Начато создание пользователя. Получен объект: " + createuser);
+		User responseUser = userRepository.createUser(createuser);
+		log.info("Создан пользователь: " + responseUser);
+		
+		log.info("Начато преобразование responseUser в объект класса ResponseUserDto. Получен объект: " + responseUser);
+		ResponseUserDto responseUserDto = UserMapper.userToResponseUserDto(responseUser);
+		log.info("createUserDto преобразован в объект класса User: " + createuser);
+		
+		return responseUserDto;
 	}
 
 	public ResponseUserDto updateUser(UpdateUserDto updateUserDto) {
 		Long userId = updateUserDto.getUserId();
 		String email = updateUserDto.getEmail();
-
+		
 		String errorMessage = "Невозможно обновить пользователя.";
 		userException.checkUserNotFoundException(userId, errorMessage);
-		userException.checkEmailAlreadyExistsException(email, errorMessage);
-		return userRepository.updateUser(updateUserDto);
+		if (userRepository.isUserOwnerOfEmail(userId, email) == false ) {
+			userException.checkEmailAlreadyExistsException(email, errorMessage);
+		}
+
+		log.info("Начато преобразование updateUserDto в объект класса User. Получен объект: " + updateUserDto);
+		User user = UserMapper.updateUserDtoUser(updateUserDto);
+		log.info("createUserDto преобразован в объект класса User: " + user);
+
+		User responseUser = userRepository.updateUser(updateUserDto);
+		ResponseUserDto responseUserDto = ResponseUserDtoMapper.toResponseUserDto(user);
 	}
 
 	public ResponseUserDto getUser(Long userId) {
 		String errorMessage = "Невозможно получить пользователя.";
 		userException.checkUserNotFoundException(userId, errorMessage);
+
 
 		log.info("Начато получение пользователя. Получен id: " + userId);
 		ResponseUserDto getedUser = userRepository.getUser(userId);
