10149f7abb925c7ad40b554fad66eb7d5889efa5
Egor Malyshev <ya.krumm@ya.ru>
2025-08-17 12:43:37 +0300

    #5 добавлен класс Comment и реализованы связи с Users и Items

--- src/main/java/ru/practicum/shareit/item/mvc/model/Comment.java
+++ src/main/java/ru/practicum/shareit/item/mvc/model/Comment.java
@@ -1,43 +1,41 @@
-package ru.practicum.shareit.user.mvc.model;
-
-import java.util.List;
+package ru.practicum.shareit.item.mvc.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
-import ru.practicum.shareit.item.mvc.model.Item;
+import ru.practicum.shareit.user.mvc.model.User;
 
 @Entity
 @Getter
 @Setter
 @Builder
-@ToString
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Table(name = "users")
-public class User {
+@Table(name = "comments")
+public class Comment {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "user_id")
+	@Column(name = "comment_id")
 	private Long id;
 
-	@Column(name = "user_name")
-	private String name;
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User commentator;
 
-	@Column(name = "user_email")
-	private String email;
+	@ManyToOne
+	@JoinColumn(name = "item_id")
+	private Item item;
 
-	@OneToMany(mappedBy = "owner")
-	private List<Item> items;
+	private String comment;
 }
