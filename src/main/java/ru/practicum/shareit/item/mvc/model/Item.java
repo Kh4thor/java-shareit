2f7fd6ba846662d8b36d9de6237243957d64f870
Egor Malyshev <ya.krumm@ya.ru>
2025-08-17 12:43:37 +0300

    #5 добавлен класс Comment и реализованы связи с Users и Items

--- src/main/java/ru/practicum/shareit/item/mvc/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/mvc/model/Item.java
@@ -1,5 +1,7 @@
 package ru.practicum.shareit.item.mvc.model;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -47,6 +50,9 @@ public class Item {
 	@OneToOne
 	@JoinColumn(name = "request_id")
 	private ItemRequest itemRequest;
+
+	@OneToMany(mappedBy = "item")
+	private List<Comment> coments;
 
 	@Override
 	public String toString() {
