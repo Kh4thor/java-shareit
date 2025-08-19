647b0c4b759f8755ce3904de9fb4afb8f6d2ed08
Egor Malyshev <ya.krumm@ya.ru>
2025-08-19 20:29:18 +0300

    #14 добавлен метод получения предмета с его комментариями

--- src/main/java/ru/practicum/shareit/item/utills/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/utills/ItemMapper.java
@@ -1,12 +1,17 @@
 package ru.practicum.shareit.item.utills;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 import lombok.extern.slf4j.Slf4j;
 import ru.practicum.shareit.item.dto.CreateItemDto;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.dto.ResponseCommentDto;
 import ru.practicum.shareit.item.dto.ResponseItemDto;
 import ru.practicum.shareit.item.dto.UpdateItemDto;
+import ru.practicum.shareit.item.mvc.model.Comment;
 import ru.practicum.shareit.item.mvc.model.Item;
 
 @Slf4j
@@ -15,10 +20,15 @@ public class ItemMapper {
 
 	public static ResponseItemDto itemToResponseItemDto(Item item) {
 		
+		List<Comment> commentsList = item.getComments();
+
+		List<ResponseCommentDto> commentsDto = commentsList==null ? Collections.emptyList() :
+				commentsList.stream().map(CommentMapper::commentToResponseCommentDto).toList();
+
 		return	ResponseItemDto.builder()
 				.id(item.getId())
 				.name(item.getName())
-				.comments(item.getComments())
+				.comments(commentsDto)
 				.available(item.getAvailable())
 				.ownerId(item.getOwner().getId())
 				.description(item.getDescription())
