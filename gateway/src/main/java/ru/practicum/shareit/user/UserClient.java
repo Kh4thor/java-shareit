f1431776265d23be9f856560d791da201ea24a35
Egor Malyshev <ya.krumm@ya.ru>
2025-09-14 14:42:47 +0300

    #11 добавлены UserController и UserClient для gateway

---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserClient.java
@@ -0,0 +1,49 @@
+package ru.practicum.shareit.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.shareit.client.BaseClient;
+import ru.practicum.shareit.user.dto.CreateUserDto;
+import ru.practicum.shareit.user.dto.UpdateUserDto;
+
+public class UserClient extends BaseClient {
+    private static final String API_PREFIX = "/users";
+
+    @Autowired
+    public UserClient(@Value("${shareit-server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl + API_PREFIX))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
+    }
+
+    public ResponseEntity<Object> createUser(CreateUserDto createUserDto) {
+        return post ("",createUserDto);
+    }
+
+    public ResponseEntity<Object> updateUser (Long userId, UpdateUserDto updateUserDto){
+        return patch("/"+userId,  updateUserDto);
+    }
+
+    public ResponseEntity<Object> getUser (Long userId){
+        return get("/"+ userId);
+    }
+
+    public void deleteUser (Long userId){
+        delete("/"+ userId);
+    }
+
+    public ResponseEntity<Object> getAllUsers () {
+        return get("/");
+    }
+
+    public void deleteAllUsers() {
+        delete ("");
+    }
+}
---
+++ gateway/src/main/java/ru/practicum/shareit/user/UserController.java
@@ -0,0 +1,61 @@
+package ru.practicum.shareit.user;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.item.dto.CreateCommentDto;
+import ru.practicum.shareit.item.dto.CreateItemDto;
+import ru.practicum.shareit.item.dto.UpdateItemDto;
+import ru.practicum.shareit.user.dto.CreateUserDto;
+import ru.practicum.shareit.user.dto.ResponseUserDto;
+import ru.practicum.shareit.user.dto.UpdateUserDto;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequestMapping(path = "/users")
+@RequiredArgsConstructor
+@Validated
+public class UserController {
+    private final UserClient userClient;
+
+    @PostMapping
+    public ResponseEntity<Object> createUser(@RequestBody CreateUserDto createUserDto) {
+        return userClient.createUser(createUserDto);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> updateUser(
+            @RequestBody UpdateUserDto updateUserDto,
+            @PathVariable("id") Long userId) {
+        updateUserDto.setUserId(userId);
+        return userClient.updateUser(userId, updateUserDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getUser(@PathVariable("id") Long userId) {
+        return userClient.getUser(userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long userId) {
+        userClient.deleteUser(userId);
+    }
+
+    @GetMapping
+    public ResponseEntity<Object> getAllUsers() {
+        return userClient.getAllUsers();
+    }
+
+    @DeleteMapping
+    public void deleteAllUsers() {
+        userClient.deleteAllUsers();
+    }
+}
